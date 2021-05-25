a03c66b09bc6c1d0f29a4ece53f248d3bfb2f1df
Jeff North <jnorth@tql.com>
2021-05-25 09:36:27 -0400

    Add project files.

---
+++ FibSequenceLesson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FibSequenceLesson
+{
+    class Program
+
+    /*You are to calculate the average of the sequence of the fibinacci sequence that
+ that ends with the first number greater than 200.  Store the generate numbers
+in a List<int> collection then calculate the average and display it using a Console.Writeline
+statement
+    code sample
+    var x = 0;
+    var y = 1;
+    var z = x + y;
+    x = 1;  this was y value
+    y = 1;  this was z value
+
+    x = y;
+    y = z;
+
+        */
+    {
+        static void Main(string[] args)
+        {
+            List <> = new List()
+
+
+        
+            Console.WriteLine($"The average of the Fib Seq is ")
+        }
+    }
+
