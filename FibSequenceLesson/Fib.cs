a7a589138df2d28a2cc366b47c9481adc731addc
Jeff North <jnorth@tql.com>
2021-05-25 10:00:07 -0400

    complete

---
+++ FibSequenceLesson/Fib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibSequenceLesson
+{
+    class Fib
+    {
+        //method static is a variable, but this is a static method (properties can be static as well)
+        //this is a static method   you can execute them without creating a sequence of a class
+        public static List<int> GenerateSequence(int last = 200)
+        {
+            var fibs = new List<int>();
+            var a = 0;
+            fibs.Add(a);
+            var b = 1;
+            fibs.Add(b);
+            int c = 0;
+            while(c < last)
+            {
+                c = a + b;
+                fibs.Add(c);
+                a = b;
+                b = c;
+            }
+            return fibs;
+        }
+    }
+}
--- FibSequenceLesson/Program.cs
+++ FibSequenceLesson/Program.cs
@@ -1,32 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FibSequenceLesson
-{
+{ 
     class Program
+    { 
 
-    /*You are to calculate the average of the sequence of the fibinacci sequence that
- that ends with the first number greater than 200.  Store the generate numbers
+/*You are to calculate the average of the sequence of the fibinacci sequence that
+that ends with the first number greater than 200.  Store the generate numbers
 in a List<int> collection then calculate the average and display it using a Console.Writeline
 statement
-    code sample
-    var x = 0;
-    var y = 1;
-    var z = x + y;
-    x = 1;  this was y value
-    y = 1;  this was z value
+code sample
+var x = 0;
+var y = 1;
+var z = x + y;
+x = 1;  this was y value
+y = 1;  this was z value
 
-    x = y;
-    y = z;
+x = y;
+y = z;
 
-        */
-    {
-        static void Main(string[] args)
+    */
+    static void Main(string[] args)
+    { 
+        List<int> fibs = Fib.GenerateSequence(200);
+        var sum = 0;
+        foreach (var fib in fibs)
+            sum = sum + fib;
         {
-            List <> = new List()
-
-
-        
-            Console.WriteLine($"The average of the Fib Seq is ")
+            var avg = sum / fibs.Count;
+            Console.WriteLine($"The average of the Fib Seq is {avg} ");
         }
+
+
     }
+    }
+}
 
